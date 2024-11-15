036482850c6ad7ff193346c8cda290d4421c560e
magistrigusti <magisrigusti@gmail.com>
2024-11-15 19:06:14 +0200

    first commit

---
+++ src/components/MobileMenu.tsx
@@ -0,0 +1,13 @@
+const MobileMenu = () => {
+  return (
+    <div className="">
+      <div className="flex flex-col gap-[4.5] cursor-pointer">
+        <div className="w-6 h-1 bg-blue-500 rounded-sm" />
+        <div className="w-6 h-1 bg-blue-500 rounded-sm" />
+        <div className="w-6 h-1 bg-blue-500 rounded-sm" />
+      </div>
+    </div>
+  )
+};
+
+export default MobileMenu;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import MobileMenu from './MobileMenu';
+
+const Navbar = () => {
+  return (
+    <div className="h-24 flex items-center justify-between">
+      <div className="">
+        <Link className="font-bold text-xl text-blue-500" href='/'>JOKER NETWORK</Link>
+      </div>
+      <div className="hidden"></div>
+      <div className="">
+        <MobileMenu />
+      </div>
+    </div>
+  )
+};
+
+export default Navbar;
