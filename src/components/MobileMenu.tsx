f830d98cf1738ebd50ad97c3af85155c9d8270e8
magistrigusti <magisrigusti@gmail.com>
2024-11-21 15:24:39 +0200

    0.01b

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -6,19 +6,20 @@ const MobileMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="">
+    <div className="md:hidden">
       <div className="flex flex-col gap-[4.5] cursor-pointer"
         onClick={() => setIsOpen((prev) => !prev)}
       >
         <div className={`w-6 h-1 bg-blue-500 rounded-sm ${
           isOpen ? "rotate-45" : ""
-        } origin-left`} />
+        } origin-left ease-in-out duration-500`} />
         <div className={`w-6 h-1 bg-blue-500 rounded-sm ${
-          isOpen ? "opacity-0" : ""}`
+          isOpen ? "opacity-0" : ""
+        } ease-in-out duration-500`
         } />
         <div className={`w-6 h-1 bg-blue-500 rounded-sm ${
           isOpen ? "-rotate-45" : ""
-        } origin-left`} />
+        } origin-left ease-in-out duration-500`} />
         
       </div>
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,36 @@
 import Link from "next/link";
 import MobileMenu from './MobileMenu';
+import Image from "next/image";
 
 const Navbar = () => {
   return (
     <div className="h-24 flex items-center justify-between">
-      <div className="">
-        <Link className="font-bold text-xl text-blue-600" href='/'>JOKER NETWORK</Link>
+      <div className="md:hidden lg:block w-[20%]">
+        <Link className="font-bold text-xl text-blue-600" href='/'>
+          JOKER NETWORK
+        </Link>
       </div>
-      <div className="hidden"></div>
-      <div className="">
+
+      <div className="hidden md:flex w-[50%] text-sm">
+        <div className="flex gap-6 text-gray-600">
+          <Link className="flex items-center gap-2" href="/">
+            <Image className="w-4 h-4" src="/home.png" alt="Homepage" width={16} height={16} />
+            <span>Homepage</span>
+          </Link>
+
+          <Link className="flex items-center gap-2" href="/">
+            <Image className="w-4 h-4" src="/friends.png" alt="Friendspage" width={16} height={16} />
+            <span>Friendspage</span>
+          </Link>
+
+          <Link className="flex items-center gap-2" href="/">
+            <Image className="w-4 h-4" src="/stories.png" alt="Storiespage" width={16} height={16} />
+            <span>Storiespage</span>
+          </Link>
+        </div>
+      </div>
+
+      <div className="flex items-center gap-4 xl:gap-8 justify-end w-[30%]">
         <MobileMenu />
       </div>
     </div>
